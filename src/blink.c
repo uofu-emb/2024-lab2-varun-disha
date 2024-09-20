88e5d831ffe32799ccd6a15a7407cd83611b29ec
VarunBhavya <varu.varun1995@gmail.com>
2024-09-19 18:11:03 -0600

    Added char testing function

--- src/blink.c
+++ src/blink.c
@@ -13,13 +13,19 @@
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
 
+//Header file created to use definitions of led_pin() and read_char()
+#include "blink.h"
+
 int count = 0;
 bool on = false;
 
+//Defines task priority 
 #define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
 #define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
-#define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
-#define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+
+//Defines the task size to be minimal in stack memory
+#define MAIN_TASK_STACK_SIZE    configMINIMAL_STACK_SIZE
+#define BLINK_TASK_STACK_SIZE   configMINIMAL_STACK_SIZE
 
 void blink_task(__unused void *params) {
     hard_assert(cyw43_arch_init() == PICO_OK);
@@ -32,7 +38,7 @@ void blink_task(__unused void *params) {
 
 void main_task(__unused void *params) {
     xTaskCreate(blink_task, "BlinkThread",
-                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
+                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);   
     char c;
     while(c = getchar()) {
         if (c <= 'z' && c >= 'a') putchar(c - 32);
@@ -41,14 +47,23 @@ void main_task(__unused void *params) {
     }
 }
 
+
+//Main Function
 int main( void )
 {
+    //Initializing USB
     stdio_init_all();
     const char *rtos_name;
     rtos_name = "FreeRTOS";
+
+    //Creating a instance of structure for tskTaskControlBlock which is assigned to typedef struct TaskHandle_t
     TaskHandle_t task;
+
+    //xTaskCreate, take function name, task size and task priority.
     xTaskCreate(main_task, "MainThread",
                 MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+
+    //Starts the task created and runs.            
     vTaskStartScheduler();
     return 0;
 }
--- test/test.c
+++ test/test.c
@@ -3,6 +3,8 @@
 #include <stdint.h>
 #include <unity.h>
 #include "unity_config.h"
+#include "pico/cyw43_arch.h"
+//#include "blink.h"
 
 void setUp(void) {}
 
@@ -22,11 +24,63 @@ void test_multiplication(void)
     TEST_ASSERT_TRUE_MESSAGE(z == 5, "Multiplication of two integers returned incorrect value.");
 }
 
+char read_char(char c){
+    if (c <= 'z' && c >= 'a') putchar(c - 32);
+    else if (c >= 'A' && c <= 'Z') putchar(c + 32);
+    else putchar(c);
+    return c;
+}
+
+void led_pin(){
+    int count = 0;
+    bool on = false;    
+    int gpio_state = false;
+    int check_count = 0;
+
+    //Testing 10 times as 110 is multiple of 11 and checking how many times it enters the loop to verify.
+    for(int loop_count = 0; loop_count <111; loop_count ++)
+    {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
+        gpio_state = cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN);
+        TEST_ASSERT_EQUAL_MESSAGE(gpio_state, on, "TEST FAILED");
+        if(count++ % 11){
+            check_count++;
+            on = !on;
+        }
+        sleep_ms(500);
+    }
+    TEST_ASSERT_EQUAL_MESSAGE(check_count, 100, "TEST FAILED");
+}
+
+void test_read_char(){
+    char result;
+    //For lower case letters
+    for (char c = 'a'; c<='z'; c++){
+    result = read_char(c);
+    TEST_ASSERT_TRUE_MESSAGE(result = c - 32 , "TEST FAILED");
+    }
+    //For Upper case letters
+    for (char c = 'A'; c<='Z'; c++){
+    result = read_char(c);
+    TEST_ASSERT_TRUE_MESSAGE(result = c + 32 , "TEST FAILED");
+    }
+}
+
+void test_led_pin(){
+    sleep_ms(5000);
+}
+
+
 int main (void)
 {
     stdio_init_all();
+    sleep_ms(5000); // Give time for TTY to attach.
+    printf("Start tests\n");
     UNITY_BEGIN();
-    RUN_TEST(test_variable_assignment);
-    RUN_TEST(test_multiplication);
+    // RUN_TEST(test_variable_assignment);
+    // RUN_TEST(test_multiplication);
+    RUN_TEST(test_read_char);
+    //RUN_TEST(led_pin);
+    sleep_ms(5000);
     return UNITY_END();
 }
